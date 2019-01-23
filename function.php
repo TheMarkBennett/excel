e9e0aad199cb42468ac99d43c58d62fca0ab97fb
Mark Bennett <mark.bennett@ucf.edu>
2019-01-23 13:52:02 -0500

    add files

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+function sidebar_widgets_init() {
+    register_sidebar( array(
+        'name' => __( 'Main Sidebar'),
+        'id' => 'main_sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widgettitle">',
+        'after_title' => '</h3>'
+    ) );
+}
+add_action( 'widgets_init', 'sidebar_widgets_init' );
---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div class="widget-area secondary col-xs-12 col-md-3">
+  <?php if ( is_active_sidebar( 'main_sidebar' ) ) : ?>
+        <div class="sidebar-main">
+          <?php dynamic_sidebar( 'main_sidebar' ); ?>
+        </div>
+    <?php endif; ?>
+
+</div>
