47b08b3536be7e438d5b50773e8191b207780681
Mark Bennett <mark.bennett@ucf.edu>
2019-01-23 13:49:48 -0500

    initial Release

---
+++ singular.php
@@ -0,0 +1,14 @@
+<?php get_header(); the_post(); ?>
+
+<article class="<?php echo $post->post_status; ?> post-list-item">
+	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+		<div class="row">
+			<div class="col-xs-12 col-md-8">
+				<?php the_content(); ?>
+			</div>
+                <?php get_sidebar(); ?>
+		</div>
+	</div>
+</article>
+
+<?php get_footer(); ?
