9e6859cb8e521e67f7aecc6d4797a7506026ef55
Mark Bennett <mark.bennett@ucf.edu>
2019-01-29 09:13:31 -0500

    text

--- single-person.php
+++ single-person.php
@@ -2,8 +2,8 @@
 
 <article class="<?php echo $post->post_status; ?> post-list-item">
 	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
-		<div class="row">   
-               
+		<div class="row">
+
                     <div class="col-xs-12 col-md-8">
                        <h1> <?php  the_field('person_title_prefix');?> <?php the_title(); ?> <?php  the_field('person_title_suffix');?> <h1>
                        <p> <?php  the_field('person_jobtitle');?> </p>
@@ -13,15 +13,16 @@
                             </div>
                             <div class="col-xs-12 col-md-4">
                             <p> <?php  the_field('person_email');?> </p>
-                            </di>
+													</div>
                             <div class="col-xs-12 col-md-4">
                             <p> <?php  the_field('person_location');?> </p>
                         </div>
-                    </div>                    
-                    <div class="col-xs-12 col-md-4">  
-                        <?php if ( has_post_thumbnail() ) : ?>                            
-                               <?php the_post_thumbnail('medium_large', ['class' => 'img-fluid' ]); ?>                          
-                        <?php endif; ?>                  
+                    </div>
+									</div>
+                    <div class="col-xs-12 col-md-4">
+                        <?php if ( has_post_thumbnail() ) : ?>
+                               <?php the_post_thumbnail('medium_large', ['class' => 'img-fluid' ]); ?>
+                        <?php endif; ?>
 
                     </div>
         </div>
@@ -29,8 +30,8 @@
                 <div class="col-xs-12 col-md-8">
 				        <?php the_content(); ?>
 			    </div>
-            </div>            
-	
+            </div>
+
 </article>
 
 <?php get_footer(); ?>
