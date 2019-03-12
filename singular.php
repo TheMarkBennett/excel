f5fc57681a05a00b2789ac9e4a8f2ed46c0ba24d
Mark Bennett <mark.bennett@ucf.edu>
2019-03-12 16:08:11 -0400

    added layout

--- singular.php
+++ singular.php
@@ -1,6 +1,15 @@
 <?php get_header(); the_post(); ?>
 
-<?php get_template_part( 'templates/content', 'sidebar' ); ?>
+
+<?php 
+
+if ( get_post_type( get_the_ID() ) == 'person' ) {
+    get_template_part( 'templates/content', 'boxed' ); 
+}else{
+ get_template_part( 'templates/content', 'sidebar' ); 
+}
+?>
+
 
 
 <?php get_footer(); ?>
---
+++ templates/content-boxed.php
@@ -0,0 +1,7 @@
+<article class="<?php echo $post->post_status; ?> post-list-item">
+	<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+		
+				<?php the_content(); ?>
+			
+	</div>
+</article>
