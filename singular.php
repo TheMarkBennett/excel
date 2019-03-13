95575a4ebedf474a350d41daad91cee01f7c266d
Mark Bennett <mark.bennett@ucf.edu>
2019-03-13 09:30:03 -0400

    remove page title

--- singular.php
+++ singular.php
@@ -5,11 +5,22 @@
 
 if ( is_singular('person') ) {
     get_template_part( 'templates/content', 'boxed' ); 
+    add_filter( 'the_title', 'remove_page_title', 10, 2 );
 }else{
  get_template_part( 'templates/content', 'sidebar' ); 
 }
 ?>
 
-
-
 <?php get_footer(); ?>
+
+
+<?php
+
+
+function remove_page_title( $title, $id ) {
+           return '';
+
+    return $title;
+}
+
+?>
